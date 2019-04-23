fbcd624ce8f3f29ece0754b62f023625d7073de3
Ranga Chari <32967367+ranga11@users.noreply.github.com>
2019-04-23 18:13:08 -0400

    Add files via upload

---
+++ pyspark1.py
@@ -0,0 +1,16 @@
+#
+
+import sys
+import imp
+
+print(sys.executable, sys.version_info)
+for package in sys.argv[1:]:
+  print(imp.find_module(package))
+
+# COMMAND ----------
+
+#! pip install findspark
+
+# COMMAND ----------
+
+
